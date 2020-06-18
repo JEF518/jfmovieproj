273d6908afab6994cdfc575ce07177d61bf4eef9
Josie <josie.field@sky.uk>
2020-06-18 20:14:16 +0100

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+// var express = require('express');
+// var router = express.Router();
+// var allfilms = require('./allfilms');
+// var getallfilms = require('./getallfilms');
+// /* GET home page. */
+// router.get('/', function (req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// router.get('/allfilms', getallfilms.list);
+
+// module.exports = router;
