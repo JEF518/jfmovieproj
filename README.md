9cde5b3f51bef272742b9dfac0f0766b68f04708
JEF518 <36102590+JEF518@users.noreply.github.com>
2020-10-18 13:15:09 +0100

    Jfmovieproj 3 tidy up (#2)

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # jfmovieproj
- 
+
 A node project that reads from a JSON file of 'movies',
 retrieving richer data on the movie from an api and grabbing
 the trailer from another api.
--- routes/routes.js
+++ routes/routes.js
@@ -4,8 +4,6 @@ const watchedRoutes = require("./watched");
 const randomiseWatchedRoutes = require("./randomise-watched");
 
 const appRouter = (app, fs) => {
-  // we've added in a default route here that handles empty routes
-  // at the base API url
   watchedRoutes(app, fs);
   randomiseRoutes(app, fs);
   filmRoutes(app, fs);
