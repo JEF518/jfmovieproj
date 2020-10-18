ac1582f32bc00e2850f6575b05166aefcdb5e7ad
JEF518 <36102590+JEF518@users.noreply.github.com>
2020-10-18 12:46:39 +0100

    Adding watched flags to frontend for more clarity (#1)

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const hbs = require('hbs');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const hbs = require("hbs");
 const fs = require("fs");
-require('dotenv').config();
-
-
+require("dotenv").config();
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
-hbs.registerHelper('loud', function (aString) {
-  return aString.toUpperCase()
-})
+hbs.registerHelper("loud", function (aString) {
+  return aString.toUpperCase();
+});
 
 // catch 404 and forward to error handler
 // app.use(function (req, res, next) {
@@ -37,11 +34,11 @@ const routes = require("./routes/routes.js")(app, fs);
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/routes.js
+++ routes/routes.js
@@ -4,12 +4,12 @@ const watchedRoutes = require("./watched");
 const randomiseWatchedRoutes = require("./randomise-watched");
 
 const appRouter = (app, fs) => {
-    // we've added in a default route here that handles empty routes
-    // at the base API url
-    watchedRoutes(app, fs);
-    randomiseRoutes(app, fs);
-    filmRoutes(app, fs);
-    randomiseWatchedRoutes(app, fs);
+  // we've added in a default route here that handles empty routes
+  // at the base API url
+  watchedRoutes(app, fs);
+  randomiseRoutes(app, fs);
+  filmRoutes(app, fs);
+  randomiseWatchedRoutes(app, fs);
 };
 
 module.exports = appRouter;
